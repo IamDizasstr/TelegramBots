83ff8a26e0f8e95c9828da132c836135a147bd60
Valeriya Vasileva <109901880+IamDizasstr@users.noreply.github.com>
2023-02-27 18:42:03 +0300

    Add files via upload

---
+++ Echo_bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+# Создание экземпляра бота
+bot = telebot.TeleBot('5826859543:AAE48MF1W6n6xi_nPM_yTnA_J9VYlJelmpU')
+
+
+# Функция, которая обрабатывает команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Пиши, что хочешь)')
+
+
+# Функция получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
